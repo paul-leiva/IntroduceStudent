7d3b618c37985825ba777082fb42f17907135552
Paul Leiva <54649136+paul-leiva@users.noreply.github.com>
2023-01-22 08:06:44 -0500

    Completed Step 3: Creating Alert Box

--- IntroduceStudent/ViewController.swift
+++ IntroduceStudent/ViewController.swift
@@ -25,7 +25,35 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func stepperDidChange(_ sender: UIStepper) {
+        
+        numberOfPetsLabel.text = "\(Int(sender.value))"
+    }
 
-
+    @IBAction func introduceSelfDidTapped(_ sender: Any) {
+        
+        // Lets us choose the title we have selected from the segmented control
+        // In our example, that is whether it is first, second, third, or fourth
+        let year = yearSegmentedControl.titleForSegment(at: yearSegmentedControl.selectedSegmentIndex)
+        
+        // Creating a constant of type String that holds an introduction.
+        // The introduction receives the values from the outlet connections.
+        let introduction = " My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend \(schoolNameTextField.text!). I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) dogs. It is \(morePetsSwitch.isOn) that I want more pets."
+        
+        // print(introduction)
+        
+        // Creates the alert where we pass in our message, which is our introduction
+        let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
+        
+        // A way to dismiss the box once it pops up
+        //let alertAction = UIAlertAction(title: "Nice to meet you!", style: .default, handler: nil)
+        
+        // Passing this action to the alert controller so it can be dismissed
+        //alertController.addAction(alertAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
 }
 
